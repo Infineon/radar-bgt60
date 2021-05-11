6f71dec6a741434940358a9b64e0f53afab74b17
Jaumann  Dominik  (IFAG DES SDF SCS EPE) <Dominik.Jaumann@infineon.com>
2021-05-11 13:22:44 +0200

    Added the platform file for Arduino to support different platforms

--- src/framework/arduino/wrapper/bgt60-platf-ino.hpp
+++ src/framework/arduino/wrapper/bgt60-platf-ino.hpp
@@ -1,12 +1,19 @@
 /**
- * @file bgt60-platf-ino.hpp
- * @author your name (you@domain.com)
- * @brief
- * @version 0.1
- * @date 2021-05-10
+ * @file        bgt60-platf-ino.hpp
+ * @author      Infineon Technologies AG
+ * @brief       Radar BGT60 platform definitions
+ * @details     This file is defining the standard pins for the corresponding
+ *              platform. Please make sure to connect your chosen board to pins
+ *              mentioned here. Otherwise the functionality of the library can not
+ *              be guaranteed. There is also the possibility to changes these pins
+ *              either in this file or directly in the sketch. But make sure you're
+ *              using the correct pins and in case you want to use the interrupt
+ *              function, you also have to make sure the pins you chose are interrupt
+ *              capable.
  *
- * @copyright Copyright (c) 2021
+ * @copyright   Copyright (c) 2021 Infineon Technologies AG
  *
+ * SPDX-License-Identifier: MIT
  */
 
 #ifndef BGT60_PLATF_INO_HPP_
@@ -18,24 +25,27 @@
 
 #include <Arduino.h>
 
-#ifdef ARDUINO_SAMD_MKR1000         /**< Arduino MKR1000 */
+#ifdef ARDUINO_SAMD_MKR1000                 /**< Arduino MKR1000 */
 
 #define TD  16
 #define PD  17
 
-/** TODO: Also add all the other MKR-Boards from Arduino as soon they are tested **/
+#elif defined(ARDUINO_SAMD_MKRWIFI1010)     /**< Arduino MRK1010 */
 
-#elif defined(ARDUINO_AVR_UNO)      /**< Arduino MKR1000 */
+#define TD  16
+#define PD  17
 
-#define TD  2
-#define PD  3
-
-#elif defined(XMC1100_Boot_Kit)     /**< XMC1100 Boot Kit */
+#elif defined(ARDUINO_AVR_UNO)              /**< Arduino Uno */
 
 #define TD  2
 #define PD  3
 
-#elif defined(XMC4700_RELAX_Kit)    /**< XMC4700 Relax Kit */
+#elif defined(XMC1100_Boot_Kit)             /**< XMC1100 Boot Kit */
+
+#define TD  2
+#define PD  3
+
+#elif defined(XMC4700_Relax_Kit)            /**< XMC4700 Relax Kit */
 
 #define TD  2
 #define PD  3
@@ -47,4 +57,6 @@ supported by the library. Have a look in the Wiki for more information."
 
 #endif
 
-#endif /** BGT60_FRAMEWORK **/
+#endif /** BGT60_FRAMEWORK */
+
+#endif /** BGT60_PLATF_INO_HPP_ */
