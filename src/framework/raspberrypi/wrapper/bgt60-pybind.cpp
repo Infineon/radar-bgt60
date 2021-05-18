2000fc51cb9338120811924c95824579f5da13c1
Panday Swarnam (IFIN DES SDF SCS EPE) <Swarnam.Panday@infineon.com>
2021-05-18 15:05:22 +0530

    Pushing changes in wrapper file for raspberry

--- src/framework/raspberrypi/wrapper/bgt60-pybind.cpp
+++ src/framework/raspberrypi/wrapper/bgt60-pybind.cpp
@@ -16,14 +16,14 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(bgt60_py, m) {
-    //Wrapper for Bgt60 Class 
-    py::class_<Bgt60> bgt60(m, "Bgt60");
-    bgt60
+    // Wrapper for Bgt60 Class 
+    py::class_<Bgt60>bgt60(m, "Bgt60");
+        bgt60
         .def("init", &Bgt60::init, "Initialization function")
         .def("deinit", &Bgt60::deinit, "De-initialize function")
         .def("getMotion", &Bgt60::getMotion, "Function to detect moving target")
-        .def("getDirection", &Bgt60::getDirection, "Function to detect direction of moving target")
-        .def("enableInterrupt", &Bgt60::enableInterrupt, "Enable interrupts")
+        .def("getDirection", &Bgt60::getDirection, "Function to detect direction of \
+                                                    moving target")
         .def("disableInterrupt", &Bgt60::disableInterrupt, "Disable interrupt");
     
     // Wrapping variables that are arguements to member functions and are defined as enum
@@ -46,11 +46,22 @@ PYBIND11_MODULE(bgt60_py, m) {
         .value("DEPARTING", Bgt60::Direction_t::DEPARTING)
         .export_values();
 
-
     // Wrapper for child Bgt60Rpi class
     py::class_<Bgt60Rpi, Bgt60> bgt60rpi(m, "Bgt60Rpi", py::multiple_inheritance());
     bgt60rpi.def(py::init<uint8_t, uint8_t>());
 
-}
+    m.def("setInterrupt", [](Bgt60Rpi radarShieldObj){
+        Error_t err_int = OK;
+        err_int = radarShieldObj).enableInterrupt(cBackF);
+        return err_int;
+    });
 
+    m.def("checkIntStats", [](std::function<bool(void)> &py_callback){
+        bool flag_stat = 0;
+        if(int_flag == 1)
+            flag_stat = py_callback();
+        return flag_stat;   
+    });
+
+}
 #endif /** BGT60_FRAMEWORK **/
